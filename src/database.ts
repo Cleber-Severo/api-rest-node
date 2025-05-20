78da6517261b3fe5a11e6dd6d1d1ef313ddbef1e
Cleber Severo <clebersevero1998@gmail.com>
2025-05-19 22:41:43 -0300

    chore: production config

--- src/database.ts
+++ src/database.ts
@@ -3,15 +3,15 @@ import 'dotenv/config';
 import { env } from './env';
 
 export const config: Knex.Config = {
-	client: 'sqlite',
-	connection: {
-		filename: env.DATABASE_URL,
-	},
-	useNullAsDefault: true,
-	migrations: {
-		extension: 'ts',
-		directory: './db/migrations',
-	},
+  client: env.DATABASE_CLIENT,
+  connection: env.DATABASE_CLIENT === 'sqlite'
+    ? {filename: env.DATABASE_URL,} 
+    : env.DATABASE_URL,
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
 };
 
 export const knex = settupKnex(config);
