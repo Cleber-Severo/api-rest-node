365d06fc6fd0a22162db719f3279ca1cbfb9345d
Cleber Severo <clebersevero1998@gmail.com>
2025-05-20 21:52:51 -0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import { env } from './env';
 
 app
   .listen({
-    // host: '0.0.0.0',
-  })
+		port: env.PORT,
+	})
   .then(() => {
     console.log('HTTP Server Running on http://localhost:3333');
   });
