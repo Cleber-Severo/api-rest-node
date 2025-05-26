3fdb4355a2508abf3c48a49cba2e8380538f923f
Cléber Severo <clebersevero1998@gmail.com>
2025-05-26 18:43:13 -0300

    feat: add get users route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,70 +5,75 @@ import crypto, { randomUUID } from 'node:crypto';
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-	app.addHook('preHandler', async (request, reply) => {
-		console.log(`[${request.method}] ${request.url}`);
-	});
+  app.addHook('preHandler', async (request, reply) => {
+    console.log(`[${request.method}] ${request.url}`);
+  });
 
-	app.get('/', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
-		try {
-			const { sessionId } = request.cookies;
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
+    try {
+      const { sessionId } = request.cookies;
 
-			console.log('get sessionId: ', sessionId);
+      console.log('get sessionId: ', sessionId);
 
-			const transactions = await knex('transactions').where('session_id', sessionId).select();
+      const transactions = await knex('transactions').where('session_id', sessionId).select();
 
-			return { transactions };
-		} catch (error) {
-			console.log(error);
-		}
-	});
+      return { transactions };
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
-	app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
-		const getTransactionSchema = z.object({
-			id: z.string().uuid(),
-		});
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
+    const getTransactionSchema = z.object({
+      id: z.string().uuid(),
+    });
 
-		const { id } = getTransactionSchema.parse(request.params);
+    const { id } = getTransactionSchema.parse(request.params);
 
-		const transaction = await knex('transactions').where('id', id).first();
+    const transaction = await knex('transactions').where('id', id).first();
 
-		return { transaction };
-	});
+    return { transaction };
+  });
 
-	app.get('/summary', { preHandler: [checkSessionIdExists] }, async () => {
-		const summary = await knex('transactions').sum('amount', { as: 'amount' }).first();
+  app.get('/summary', { preHandler: [checkSessionIdExists] }, async () => {
+    const summary = await knex('transactions').sum('amount', { as: 'amount' }).first();
 
-		return { summary };
-	});
+    return { summary };
+  });
 
-	app.post('/', async (request, reply) => {
-		const createTransactionBodySchema = z.object({
-			title: z.string(),
-			amount: z.number(),
-			type: z.enum(['credit', 'debit']),
-		});
+  app.post('/', async (request, reply) => {
+    try {
+      const createTransactionBodySchema = z.object({
+        title: z.string(),
+        amount: z.number(),
+        type: z.enum(['credit', 'debit']),
+      });
 
-		const { amount, title, type } = createTransactionBodySchema.parse(request.body);
+      const { amount, title, type } = createTransactionBodySchema.parse(request.body);
 
-		let sessionId = request.cookies.sessionId;
+      let sessionId = request.cookies.sessionId;
 
-		if (!sessionId) {
-			sessionId = randomUUID();
+      if (!sessionId) {
+        sessionId = randomUUID();
 
-			reply.cookie('sessionId', sessionId, {
-				path: '/',
-				maxAge: 60 * 60 * 24 * 7, // 7 dias
-			});
-		}
+        reply.cookie('sessionId', sessionId, {
+          path: '/',
+          maxAge: 60 * 60 * 24 * 7, // 7 dias
+        });
+      }
 
-		console.log('sessionId: ', sessionId);
-		await knex('transactions').insert({
-			id: crypto.randomUUID(),
-			title,
-			amount: type === 'credit' ? amount : amount * -1,
-			session_id: sessionId,
-		});
+      console.log('sessionId: ', sessionId);
+      await knex('transactions').insert({
+        id: crypto.randomUUID(),
+        title,
+        amount: type === 'credit' ? amount : amount * -1,
+        session_id: sessionId,
+      });
 
-		return reply.status(201).send('sucesso');
-	});
+      return reply.status(201).send('sucesso');
+    } catch (error) {
+      console.log(error)
+      return reply.status(500).send('Internal Error');
+    }
+  });
 }
